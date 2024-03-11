d0df56e288c880a6ec3452413c656c6c563e8bfb
Oleg <oleg.goshovskyy@gmail.com>
2024-03-11 08:30:11 +0100

    Added factory method addFlower

--- src/Flowers/Flower.java
+++ src/Flowers/Flower.java
@@ -14,6 +14,23 @@ public abstract class Flower {
         this.chanceOfGrowth = chanceOfGrowth;
     }
 
+    public Flower addFlower(String flower) throws Exception {
+        switch (flower){
+            case "Sunflower":
+                return new Sunflower("Sunflower", 90, 6, 85);
+            case "Rose":
+                return new Rose("Rose", 100, 4,50);
+            case "Daisy":
+                return new Daisy("Daisy", 65, 4, 60);
+            case "Tulip":
+                return new Tulip("Tulip", 60, 4, 65);
+            case "Poppy":
+                return new Poppy("Poppy", 55, 4, 70);
+            default:
+                throw new Exception("Flower specification needed.");
+        }
+    }
+
     @Override
     public String toString() {
         return "Flowers.Flower{" +
